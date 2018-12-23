cd2d56bf3e27921f1bd8b5e2bc625c5e29c83e6f
Derek Borba <derekborba@gmail.com>
2018-12-22 20:49:04 -0800

    Move files from local directory to github - intial full commit

--- R/url_builder.R
+++ R/url_builder.R
@@ -15,7 +15,12 @@ url_builder <- function(station,
                     End = end_date)
   #  Endpoint
   url <- httr::build_url(url)
+
+  #  Print the assembled url to the screen
   print(url)
+
+  #  Return the url
+  return(url)
 }
 
 #url <- 'https://cdec.water.ca.gov/dynamicapp/req/JSONDataServlet?Stations=DTO&SensorNums=23&dur_code=D&Start=2018-06-13&End=2018-09-13'
